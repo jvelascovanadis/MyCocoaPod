203f3e2fc0f7701b4438ac256a4a52c90529b3b2
jvelasco <jvelasco@vanadis.es>
2015-02-27 16:31:24 +0100

    Primer commit

---
+++ MyFrameworkCocoaPod/MyFrameworkCocoaPod.h
@@ -0,0 +1,19 @@
+//
+//  MyFrameworkCocoaPod.h
+//  MyFrameworkCocoaPod
+//
+//  Created by jorge Velasco on 27/02/15.
+//  Copyright (c) 2015 Vanadis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFrameworkCocoaPod.
+FOUNDATION_EXPORT double MyFrameworkCocoaPodVersionNumber;
+
+//! Project version string for MyFrameworkCocoaPod.
+FOUNDATION_EXPORT const unsigned char MyFrameworkCocoaPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFrameworkCocoaPod/PublicHeader.h>
+
+
