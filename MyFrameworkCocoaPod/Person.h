227202ea2307d661714bb29b2373c7777d22705e
jvelasco <jvelasco@vanadis.es>
2015-02-27 20:45:25 +0100

    Added person class

---
+++ MyFrameworkCocoaPod/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MyFrameworkCocoaPod
+//
+//  Created by jorge Velasco on 27/02/15.
+//  Copyright (c) 2015 Vanadis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ MyFrameworkCocoaPod/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  MyFrameworkCocoaPod
+//
+//  Created by jorge Velasco on 27/02/15.
+//  Copyright (c) 2015 Vanadis. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
